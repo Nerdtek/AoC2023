071dc6977e1a9bb3af821c4d7c33d903cd60f696
Daniel McMillan <nerdtekkie@gmail.com>
2023-12-08 04:37:20 -0500

    Day 2 Part 2 Initial Commit

---
+++ Day2Part2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Day2Part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                if (File.Exists(args[0]))
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("Input file not file.  Check Path.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Missing file path in arguments.");
+            }
+        }
+    }
+}
