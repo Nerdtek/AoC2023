86c32c605fd8eec9ae98edc3e64ed138bdef2de1
Daniel McMillan <nerdtekkie@gmail.com>
2023-12-09 23:01:26 -0500

    Day 9 Part 1 Initial Commit

---
+++ Day9Part1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Day9Part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                if (File.Exists(args[0]))
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("Input file not found.  Check the path you provided. " + args[0]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please specify an input file path.");
+            }
+        }
+    }
+}
