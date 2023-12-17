471184a6a5e62794ef622f7818477399b0a20cf1
Daniel McMillan <nerdtekkie@gmail.com>
2023-12-17 07:06:58 -0500

    Day17Part1 Initial Commit

---
+++ Day17Part1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Day17Part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                if (File.Exists(args[0]))
+                {
+                    string[] lines = File.ReadAllLines(args[0]);
+                    Util util = new Util(lines);
+                }
+                else
+                {
+                    Console.WriteLine("Input file not found.  Check the path you provided. " + args[0]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please specify an input file path.");
+            }
+        }
+    }
+}
---
+++ Day17Part1/Util.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day17Part1
+{
+    internal class Util
+    {
+        public Util(string[] lines)
+        {
+
+        }
+    }
+}
