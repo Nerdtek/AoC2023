e17e14b382f53f7e2afc177049b3470fc45b9fcc
Daniel McMillan <nerdtekkie@gmail.com>
2023-12-18 21:46:40 -0500

    Day 5 Part 1 Initial Commit

---
+++ Day5Part1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Day5Part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                if (File.Exists(args[0]))
+                {
+                    string[] lines = File.ReadAllLines(args[0]);
+                    Util util = new Util(string[] lines);
+                }
+                else
+                {
+                    Console.WriteLine("Input file not found.  Check the path you provided. " + args[0]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please specify an input file path.");
+            }
+        }
+    }
+}
---
+++ Day5Part1/Util.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5Part1
+{
+    internal class Util
+    {
+        List<int> Seeds;
+        Dictionary<int, int> SeedToSoil;
+        Dictionary<int, int> SoilToFertilizer;
+        Dictionary<int, int> FertilizerToWater;
+        Dictionary<int, int> WaterToLight;
+        Dictionary<int, int> LightToTemperature;
+        Dictionary<int, int> TemperatureToHumidity;
+        Dictionary<int, int> HumidityToLocation;
+
+
+        public Util(string[] lines)
+        {
+            Seeds = new List<int>();
+            SeedToSoil = new Dictionary<int, int>();
+            SoilToFertilizer = new Dictionary<int, int>();
+            FertilizerToWater = new Dictionary<int, int>();
+            WaterToLight = new Dictionary<int, int>();
+            LightToTemperature = new Dictionary<int, int>();
+            TemperatureToHumidity = new Dictionary<int, int>();
+            HumidityToLocation = new Dictionary<int, int>();
+            int process = -1;
+            foreach (string line in lines)
+            {
+                
+            }
+        }
+    }
+}
